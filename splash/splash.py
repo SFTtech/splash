3b4c1aacf1df928ebb3ada3b91daad6a513d0329
Jonas Jelten <jj@sft.mx>
2015-07-21 14:03:04 +0200

    splash: style optimizations

--- splash/splash.py
+++ splash/splash.py
@@ -5,53 +5,80 @@
 #
 # usage cat loltext | ./splash.py --speed 300
 
+"""
+main speedreading algorithm routines
+
+word center calculations and file parsing happen in here.
+"""
+
 import argparse
 import sys
 import time
 
 
+def word_focus(word):
+    """
+    which character to put in the center, depending on word length.
+    """
+    if len(word) < 15:
+        return ([0]*3 + [1]*4 + [2]*4 + [3]*4)[len(word)]
+    else:
+        return 4
+
+def time_factor(word):
+    """
+    word progress time adjustment by word length.
+    """
+    default_word_length = 5.5   # this lenght gets factor 1.0
+    per_char_adjust = 0.08      # factor adjust amount per char deviation
+    if len(word) > 0:
+        return max([((len(word) - default_word_length) * per_char_adjust) + 1, 0])
+    else:
+        return 0.1              # an empty word (e.g. blank line)
+
+
 class Splash:
+    """
+    manages the reading state of one file.
+    provides functions to advance words or display them on the terminal.
+    """
+
     def __init__(self, input_file):
         self.file = input_file
         self.worditer = None
-
-    def word_focus(self, word):
-        """ which character to put in the center, depending on word length. """
-        if len(word) < 15:
-            return ([0]*3 + [1]*4 + [2]*4 + [3]*4)[len(word)]
-        else:
-            return 4
-
-    def time_factor(self, word):
-        """ word progress time adjustment. """
-        default_word_length = 5.5   # this lenght gets factor 1.0
-        per_char_adjust     = 0.08  # factor adjust amount per char deviation
-        if len(word) > 0:
-            return max([((len(word) - default_word_length) * per_char_adjust) + 1, 0])
-        else:
-            return 0.1              # an empty word (e.g. blank line)
+        self.displayed_word_count = 0
 
     def get_words(self):
+        """
+        generator for all the words in the file
+        """
         for line in self.file:
-            print("line: %s" % line)
             for word in line.split(" "):
-                print("word: %s" % word)
                 yield word.strip()
 
     def display(self, sleeptime, static_speed):
+        """
+        show the current word on the console
+        """
         self.displayed_word_count = 0
         for word in self.get_words():
             if len(word) > 0:
-                c = self.word_focus(word)
-                print("\x1b[2K%s%s\x1b[31m%s\x1b[0m%s\r" % (" " * (10 - c), word[:c], word[c], word[c+1:]), end="")
+                center = word_focus(word)
+                print("\x1b[2K%s%s\x1b[31m%s\x1b[0m%s\r" % (
+                    " " * (10 - center), word[:center],
+                    word[center], word[center+1:]), end="")
                 self.displayed_word_count += 1
             if static_speed:
                 factor = 1.0 if len(word) > 0 else 0
             else:
-                factor = self.time_factor(word)
+                factor = time_factor(word)
             time.sleep(sleeptime * factor)
 
     def advance(self, wpm):
+        """
+        advance one word in the internal iterator.
+        this is the API function you want to use.
+        """
         self.displayed_word_count = 0
 
         if not self.worditer:
@@ -59,8 +86,8 @@ class Splash:
 
         try:
             word = next(self.worditer)
-            center = self.word_focus(word)
-            delay = wpm * self.time_factor(word)
+            center = word_focus(word)
+            delay = wpm * time_factor(word)
             self.displayed_word_count += 1
             end = False
 
@@ -74,11 +101,16 @@ class Splash:
         return end, word, center, delay
 
 
-if __name__ == "__main__":
+def main():
+    """
+    main entry point for the terminal operation mode
+    """
+
     if not sys.stdout.isatty():
         raise Exception("it makes no sense writing timed output to no tty.")
 
-    parser = argparse.ArgumentParser(description='free (as in freedom) speed reading software.')
+    parser = argparse.ArgumentParser(
+        description="free (as in freedom) speedreading software.")
     parser.add_argument('--speed', "-s", type=int, default=300,
                         help="reading speed in words per minute, default: 300")
     parser.add_argument('--static-speed', "-d", action="store_true",
@@ -98,6 +130,12 @@ if __name__ == "__main__":
         pass
     finally:
         run_time = time.time() - start_time
-        print("\ndisplayed %d words in %0.2f seconds: %.2f words per minute" % (speedreader.displayed_word_count,
-                                                                                run_time,
-                                                                                (speedreader.displayed_word_count*60)/run_time))
+        print(("\ndisplayed %d words in %0.2f seconds: "
+               "%.2f words per minute") % (
+                   speedreader.displayed_word_count,
+                   run_time,
+                   (speedreader.displayed_word_count*60)/run_time))
+
+
+if __name__ == "__main__":
+    main()
