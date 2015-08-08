93c29e74eadd1682cc10f6e5aec13c0ec49c25c3
Jonas Jelten <jj@sft.mx>
2015-08-08 06:57:35 +0200

    gui: style optimizations

--- splash/gui.py
+++ splash/gui.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+gui functionality for splash.
+allows speedreading with an interactive gui.
+"""
+
 from gi.repository import (GObject as gobject,
                            Gtk as gtk,
-                           Gdk as gdk,
-                           cairo,
                            Pango as pango,
                            PangoCairo as pangocairo)
 
@@ -12,9 +15,14 @@ import os
 
 from . import splash
 
-class SplashViewer:
-    def __init__(self):
 
+class SplashViewer:
+    """
+    gtk3 gui for speedreading.
+    Contains all the state and handling for the gtk actions.
+    """
+
+    def __init__(self):
         self.builder = gtk.Builder()
         self.builder.add_from_file(os.path.join(os.path.dirname(__file__),
                                                 "layout.glade"))
@@ -23,6 +31,7 @@ class SplashViewer:
         self.window = self.builder.get_object("window0")
         self.window.resize(300, 200)
         self.window.set_position(gtk.WindowPosition.CENTER)
+        self.draw_size = (0, 0)
 
         self.window.show_all()
 
@@ -34,23 +43,39 @@ class SplashViewer:
         self.current_center = 1
         self.playing = False
 
-        self.wpm = 100 # 70  # 300 w/min
+        self.wpm = 50  # TODO: make adjustable
+        self.timer = None
 
     def run(self):
+        """
+        run the gtk main loop
+        """
         gtk.main()
 
     def open_about(self, origin):
-        d = self.builder.get_object("aboutdialog0")
-        d.show()
+        """
+        show the about window
+        """
+        dialog = self.builder.get_object("aboutdialog0")
+        dialog.show()
 
-    def close_about(self, a, b):
-        d = self.builder.get_object("aboutdialog0")
-        d.hide()
+    def close_about(self, *args):
+        """
+        hide the about window"
+        """
+        dialog = self.builder.get_object("aboutdialog0")
+        dialog.hide()
 
-    def delete_window(self, *a):
-        gtk.main_quit(a)
+    def delete_window(self, *args):
+        """
+        stop the gtk main loop when closing the window.
+        """
+        gtk.main_quit(args)
 
-    def play(self, origin):
+    def play(self, _):
+        """
+        activate playback of words for reading.
+        """
         if not self.splash:
             print("No text file loaded")
 
@@ -62,10 +87,16 @@ class SplashViewer:
         self.playing = True
         self.advance_word()
 
-    def pause(self, origin):
+    def pause(self, _):
+        """
+        suspend playing and allow continuation later.
+        """
         self.playing = False
 
     def advance_word(self, event=None):
+        """
+        show the next word of the text.
+        """
         if not self.splash:
             self.playing = False
             return False
@@ -88,10 +119,16 @@ class SplashViewer:
         return False
 
     def back(self, origin):
+        """
+        reset the reading progress and start over.
+        """
         self.playing = False
         self.splash = None
 
     def canvas_draw(self, widget, cr):
+        """
+        draw the word centered in the window.
+        """
         w, h = self.draw_size
 
         # center of the text
@@ -113,7 +150,11 @@ class SplashViewer:
 
         center = self.current_center
         if len(txt) > 0:
-            markup = "%s<span foreground='red'>%s</span>%s" % (txt[:center], txt[center], txt[center+1:])
+            markup = "%s<span foreground='red'>%s</span>%s" % tuple(
+                t.replace("<", "&lt;").replace(">", "&gt;")
+                for t in (txt[:center],
+                          txt[center],
+                          txt[center+1:]))
             prefix = txt[:center]
         else:
             markup = ""
@@ -128,7 +169,7 @@ class SplashViewer:
         pangocairo.update_layout(cr, prefixlayout)
 
         # text metrics
-        txtw, txth = (x/1024. for x in layout.get_size())
+        _, txth = (x/1024. for x in layout.get_size())
         prew, _ = (x/1024. for x in prefixlayout.get_size())
 
         cr.move_to(cx - (prew), cy - txth/2)
@@ -137,6 +178,9 @@ class SplashViewer:
         pangocairo.show_layout(cr, layout)
 
     def draw_size_change(self, widget, rectangle):
+        """
+        when the window size changes, update the stored dimensions.
+        """
         self.draw_size = (rectangle.width, rectangle.height)
 
     def open_text(self, origin):
@@ -150,8 +194,8 @@ class SplashViewer:
                                                  gtk.ResponseType.CANCEL,
                                                  gtk.STOCK_OPEN,
                                                  gtk.ResponseType.OK))
-        for f in ("textfilefilter", "anyfilefilter"):
-            filefilter = self.builder.get_object(f)
+        for ff in ("textfilefilter", "anyfilefilter"):
+            filefilter = self.builder.get_object(ff)
             chooser.add_filter(filefilter)
 
         response = chooser.run()
@@ -178,25 +222,32 @@ class SplashViewer:
 
         chooser = gtk.FileChooserDialog(title="Save reading state...",
                                         action=gtk.FileChooserAction.SAVE,
-                                         buttons=(gtk.STOCK_CANCEL,
-                                                  gtk.ResponseType.CANCEL,
-                                                  gtk.STOCK_SAVE,
-                                                  gtk.ResponseType.OK))
+                                        buttons=(gtk.STOCK_CANCEL,
+                                                 gtk.ResponseType.CANCEL,
+                                                 gtk.STOCK_SAVE,
+                                                 gtk.ResponseType.OK))
 
         response = chooser.run()
 
-    def save_state(self, origin):
-        pass
-
     def update_canvas(self):
+        """
+        when the window requires redrawing,
+        this method is called.
+        """
         canvas = self.builder.get_object("drawingarea0")
         canvas.queue_draw()
 
+
 def main():
+    """
+    create the window and display it.
+    """
+
     import signal
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     v = SplashViewer()
     v.run()
+
 
 if __name__ == "__main__":
     main()
